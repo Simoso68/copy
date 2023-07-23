52a57642ac13a6667d88e25bfcce5d7661303f9e
Simoso68 <87707341+Simoso68@users.noreply.github.com>
2023-07-23 11:43:58 +0200

    added configs, speed increase

---
+++ config_loader.py
@@ -0,0 +1,63 @@
+from json import loads
+from platform import system
+from os import getenv
+from sys import exit
+
+def replaceCFG(location):
+    replacement = """
+{
+    "progressbar": {
+        "bar": {
+            "completed":"#",
+            "incomplete":"-"
+        },
+        "container": {
+            "opening":"[",
+            "closing":"]"
+        }
+    },
+    "backend": {
+        "chars-per-iteration":256
+    }
+}""".replace("\n", "", 1)
+    try:
+        open(location, "w").write(replacement)
+    except Exception as x:
+        print("copy: not able to replace configuration file")
+        print(f"Given information: {x}")
+        exit()
+
+def readfile(path):
+    try:
+        with open(path, "r") as conf:
+            j = loads(conf.read())
+            vallist = []
+            vallist.append(j["progressbar"]["bar"]["completed"])
+            vallist.append(j["progressbar"]["bar"]["incomplete"])
+            vallist.append(j["progressbar"]["container"]["opening"])
+            vallist.append(j["progressbar"]["container"]["closing"])
+            vallist.append(j["backend"]["chars-per-iteration"])
+            return vallist
+    except FileNotFoundError:
+        print(f"copy: config file does not exist at {path}, creating a new configuration file ...")
+        replaceCFG(path)
+    except KeyError:
+        print(f"copy: configuration file at {path} is invalid, values are missing")
+    except Exception as x:
+        print(f"copy: unknown exception during configuration replacement creation. \ncopy: Given information: {x}")
+        exit()
+
+def parseCFG():
+    if system() == "Windows":
+        conffile = "C:/Users/" + getenv("USERNAME") + "/.copy.conf.json"
+        return readfile(conffile)
+    elif system() == "Linux" or system() == "Darwin":
+        if getenv("USER") == "root":
+            conffile = "/root/.copy.conf.json"
+        else:
+            conffile = "/home/" + getenv("USER") + "/.copy.conf.json"
+
+        return readfile(conffile)
+    else:
+        print("copy: was not able to detect your operating system, using default config.")
+        return ["#", "-", "[", "]", 256]
--- copy_lib.py
+++ copy_lib.py
@@ -1,16 +1,18 @@
 from sys import stdout
 from os import get_terminal_size
+from config_loader import *
+
+conf = parseCFG()
 
 def progress(cur, total):
     percent = cur / total * 100
     percent = float(percent)
-    minus = "-"
     width = get_terminal_size().columns
     if percent == 0:
-        stdout.write("\r[" + "-" * (int(width) - 9) + "] | 0%")
+        stdout.write(f"\r{conf[2]}" + conf[1] * (int(width) - 9) + f"{conf[3]} | 0%")
     else:
         fill_multiplier = int((width - 9) / (100 / percent))
-        stdout.write("\r[" + f"{minus * (width - 11)}".replace("-", "#", fill_multiplier) + "] | XXX%".replace("XXX", str(round(percent, 2))))
+        stdout.write(f"\r{conf[2]}" + f"{conf[1] * (width - 11)}".replace(conf[1], conf[0], fill_multiplier) + f"{conf[3]} | XXX%".replace("XXX", str(round(percent, 2))))
     stdout.flush()
 
 def copy_paste(file: str, outfiles: list):
@@ -20,13 +22,14 @@ def copy_paste(file: str, outfiles: list):
 
     for outfile in outfiles:
         cache.seek(0, 0)
+        chunksize = conf[4]
         with open(outfile, "wb") as o:
             while True:
-                chunk = cache.read(128)
+                chunk = cache.read(chunksize)
                 if not chunk:
                     break
                 progress(progresscount, length)
                 o.write(chunk)
-                progresscount += 128
-    stdout.write("\r[" + "#" * int(get_terminal_size().columns - 9) + "] | 100%")
+                progresscount += chunksize
+    stdout.write(f"\r{conf[2]}" + conf[0] * int(get_terminal_size().columns - 9) + f"{conf[3]} | 100%")
     stdout.flush()
